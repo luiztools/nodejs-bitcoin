41709d9a34c201136457709ee71acbf6cee7d738
luiztools <luizfduartejr@gmail.com>
2021-04-25 20:31:28 -0300

    partes 2 e 3

--- binance/api.js
+++ binance/api.js
@@ -1,7 +1,51 @@
 const axios = require('axios');
 const queryString = require('querystring');
 
-async function publicCall(path, data, method = 'GET', headers = {}) {
+const crypto = require('crypto');
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+const apiUrl = process.env.API_URL;
+
+async function accountInfo() {
+    return privateCall('/v3/account');
+}
+
+async function newOrder(symbol, quantity, price, side = 'BUY', type = 'MARKET') {
+    const data = { symbol, side, type, quantity };
+
+    if (price) data.price = parseInt(price);
+    if (type === 'LIMIT') data.timeInForce = 'GTC';
+
+    return privateCall('/v3/order', data, 'POST');
+}
+
+async function privateCall(path, data = {}, method = 'GET') {
+    if (!apiKey || !apiSecret)
+        throw new Error('Preencha corretamente sua API KEY e SECRET KEY');
+
+    const timestamp = Date.now();
+
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(`${queryString.stringify({ ...data, timestamp })}`)
+        .digest('hex');
+
+    const newData = { ...data, timestamp, signature };
+    const qs = `?${queryString.stringify(newData)}`;
+
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        });
+        return result.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function publicCall(path, data, method = 'GET') {
     try {
         const qs = data ? `?${queryString.stringify(data)}` : '';
         const result = await axios({
@@ -26,9 +70,9 @@ async function depth(symbol = 'BTCBRL', limit = 5) {
     return publicCall('/v3/depth', { symbol, limit });
 }
 
-async function exchangeInfo(symbol = 'BTCBRL') {
+async function exchangeInfo(symbol) {
     const result = await publicCall('/v3/exchangeInfo');
-    return result.symbols.find(s => s.symbol === symbol);
+    return symbol ? result.symbols.find(s => s.symbol === symbol) : result.symbols;
 }
 
-module.exports = { ping, time, depth, exchangeInfo }
+module.exports = { ping, time, depth, exchangeInfo, accountInfo, newOrder }
--- binance/index.js
+++ binance/index.js
@@ -1,7 +1,35 @@
 //index.js
 const api = require('./api');
+const profitability = parseFloat(process.env.PROFITABILITY);
+const symbol = process.env.SYMBOL;
+
 console.log('Iniciando monitoramento!');
 setInterval(async () => {
+    //console.log(await api.exchangeInfo());
     //console.log(await api.time());
-    console.log(await api.depth());
+    console.log(`Mercado para ${symbol}`);
+    const mercado = await api.depth(symbol);
+    console.log(mercado.bids.length ? `Compra: ${mercado.bids[0][0]}` : 'Sem Compras');
+    console.log(mercado.asks.length ? `Venda: ${mercado.asks[0][0]}` : 'Sem Vendas');
+
+    console.log('Carteira');
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => symbol.indexOf(b.asset) !== -1);
+    console.log(coins);
+
+    const sellPrice = parseFloat(mercado.asks[0][0]);
+    //const carteiraBUSD = parseFloat(coins.find(c => c.asset === 'BUSD').free);
+    if (sellPrice < 1000) {
+        console.log('Preço está bom. Comprando!');
+        const buyOrder = await api.newOrder(symbol, 1);
+        //console.log(buyOrder);//descomente para investigar problemas
+        console.log(`orderId: ${buyOrder.orderId}`);
+        console.log(`status: ${buyOrder.status}`);
+
+        console.log(`Posicionando venda. Ganho de ${profitability}`);
+        const sellOrder = await api.newOrder(symbol, 1, sellPrice * profitability, 'SELL', 'LIMIT');
+        //console.log(sellOrder);//descomente para investigar problemas
+        console.log(`orderId: ${sellOrder.orderId}`);
+        console.log(`status: ${sellOrder.status}`);
+    }
 }, process.env.CRAWLER_INTERVAL);
