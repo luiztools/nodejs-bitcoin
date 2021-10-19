f8c859922906a8cb26cfbda7329aa3e095331804
luiztools <luizfduartejr@gmail.com>
2021-10-19 09:08:14 -0300

    bugfixes

--- launchbot/index.js
+++ launchbot/index.js
@@ -4,10 +4,17 @@ const api = require('./api');
 const WebSocket = require('ws');
 const ws = new WebSocket("wss://stream.binance.com:9443/ws/!bookTicker");
 
+//escolha o symbol que vai monitorar - set the symbol to monitor
 const SYMBOL = 'BTCUSDT';
+//escolha o percentual de lucratividade - set the profit percent
 const profit = 1.1;
+//escolha quanto quer comprar - set the quantity to buy
+const buyQty = 0.001;
+
+//não mexa nestas variáveis
 let quantity = 0;
 let buyPrice = 0;
+let isBought = false;
 
 ws.onmessage = async (event) => {
     process.stdout.write('\033c');
@@ -21,14 +28,14 @@ ws.onmessage = async (event) => {
         console.log(`Target Price: ${buyPrice * profit}`);
 
         if (obj.s === SYMBOL) {
-            if (!buyPrice) {
+            if (!isBought) {
                 isBought = true;
-                const order = await api.newOrder(SYMBOL, 0.001, 0, 'BUY', 'MARKET');
+                const order = await api.newOrder(SYMBOL, buyQty, 0, 'BUY', 'MARKET');
                 quantity = parseFloat(order.executedQty);
                 buyPrice = parseFloat(order.fills[0].price);
                 return;
             }
-            else if (parseFloat(obj.b) > (buyPrice * profit)) {
+            else if (quantity > 0 && parseFloat(obj.b) > (buyPrice * profit)) {
                 const order = await api.newOrder(SYMBOL, quantity, 0, 'SELL', 'MARKET');
                 console.log(`Sold at ${new Date()} by ${order.fills[0].price}`);
                 process.exit(1);
