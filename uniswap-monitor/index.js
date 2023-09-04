89bd3137965feb9a6e247984f181b3bff8f45982
LuizTools <luizfduartejr@gmail.com>
2023-09-04 11:04:07 -0300

    uniswap monitor

---
+++ uniswap-monitor/index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const { INTERVAL, FACTORY_ADDRESS, QUOTER_ADDRESS, TOKEN_IN_ADDRESS, TOKEN_OUT_ADDRESS, NETWORK, INFURA_API_KEY } = process.env;
+
+const { ChainId, Token } = require("@uniswap/sdk-core");
+const WETH_TOKEN = new Token(ChainId.MAINNET, TOKEN_IN_ADDRESS, 18, 'WETH', 'Wrapped Ether');
+const USDT_TOKEN = new Token(ChainId.MAINNET, TOKEN_OUT_ADDRESS, 6, 'USDT', 'Tether');
+
+const { ethers } = require("ethers");
+const provider = new ethers.InfuraProvider(NETWORK, INFURA_API_KEY);
+
+async function preparationCycle() {
+
+    const { computePoolAddress, FeeAmount } = require("@uniswap/v3-sdk")
+    const currentPoolAddress = computePoolAddress({
+        factoryAddress: FACTORY_ADDRESS,
+        tokenA: WETH_TOKEN,
+        tokenB: USDT_TOKEN,
+        fee: FeeAmount.MEDIUM,
+    })
+
+    const IUniswapV3PoolABI = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
+    const poolContract = new ethers.Contract(currentPoolAddress, IUniswapV3PoolABI.abi, provider);
+
+    const [token0, token1, fee] = await Promise.all([
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+    ])
+
+    return { token0, token1, fee };
+}
+
+async function executionCycle(token0, token1, fee) {
+    const Quoter = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
+    const quoterContract = new ethers.Contract(QUOTER_ADDRESS, Quoter.abi, provider);
+    const quotedAmountOut = await quoterContract.quoteExactInputSingle.staticCall(
+        token0,
+        token1,
+        fee,
+        ethers.parseEther("1"),
+        0
+    )
+
+    console.log("WETH 1 is equals to USDT " + ethers.formatUnits(quotedAmountOut, 6));
+}
+
+(async () => {
+    const { token0, token1, fee } = await preparationCycle();
+
+    setInterval(() => executionCycle(token0, token1, fee), INTERVAL);
+
+    executionCycle(token0, token1, fee);
+})();
